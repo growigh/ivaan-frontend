80d0aaf847893764e85b125830043e112a014e1f
Ayush Kumar <kumarayushkumar@protonmail.com>
2025-08-15 17:25:41 +0530

    chore: add configuration files and update project structure
    - Create .editorconfig for consistent coding styles across editors
    - Add .prettierrc for code formatting rules
    - Introduce components.json for component configuration
    - Update next.config.ts to use single quotes
    - Add new dependencies in package.json and package-lock.json
    - Include favicon and apple-touch-icon images
    - Enhance globals.css with new CSS variables and dark mode support
    - Refactor layout.tsx to include meta tags and site icons
    - Simplify page.tsx to a basic structure for testing
    - Create site.webmanifest for PWA support
    - Add web app manifest images
    - Implement utility functions in utils.ts for class name management
    - Define constants in constant.ts for site configuration

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./ui/button";
+
+export default function Header() {
+  return (
+    <header className="justify-between flex py-8 container mx-auto">
+      <div className="text-4xl font-logo tracking-[15%]">EDITH</div>
+      <Button variant="secondary">Early Access</Button>
+    </header>
+  )
+}
---
+++ src/utils/constant.ts
@@ -0,0 +1,10 @@
+const CONST = {
+  GA_ID: 'G-T8VDM4N602',
+  SITE_NAME: 'Edith',
+  DESCRIPTION: 'Edith, your personal AI meeting assistant that remembers, connects and talks about all your meetings.',
+  TITLE: 'Edith',
+  SITE_URL: 'https://heyedith.in',
+  CANONICAL_URL: 'https://heyedith.in'
+}
+
+export default CONST
